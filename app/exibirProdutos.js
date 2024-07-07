a6a1faa19340b2f0191d64ef19e367829ad841da
Cesar Augusto Tomm <czar.df.69@gmail.com>
2024-07-07 00:12:30 -0300

    ajustes CSS: cores e alinhamentos

--- app/exibirProdutos.js
+++ app/exibirProdutos.js
@@ -5,7 +5,7 @@ function exibirProdutosNaTela(listaDeProdutos) {
   listaDeProdutos.forEach(produto => {
     let disponibilidade = produto.disponivel == 'sim' ? ' ' : ' indisponivel'
     elementoParaInserir.innerHTML += `
-        <div class="p-2 produto">
+        <div class="shadow-sm m-2 produto">
           <img class="produto__imagens${disponibilidade}" src="${produto.imagem}"
             alt="${produto.alt}" />
           <h2 class="produto__nome">
@@ -14,10 +14,7 @@ function exibirProdutosNaTela(listaDeProdutos) {
           <h2 class="produto__preco${disponibilidade}">
             R$${produto.preco.toFixed(2)}
           </h2>
-          <div class="tags">
-            <span class="tag">#${produto.tipo}</span>
-          </div>
         </div>
-        `
+      `
   })
 }
